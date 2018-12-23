825282f810a4836586bf86d22a21533cb65a87a4
Rhonstin <rhonstin@gmail.com>
2018-12-24 01:46:31 +0300

    Update PassWordSaver.py

--- PassWordSaver/PassWordSaver/PassWordSaver.py
+++ PassWordSaver/PassWordSaver/PassWordSaver.py
@@ -1,5 +1,13 @@
 #Программа для незащищенного хранения паролей.
-PASSWORDS = {"email" : "HEHEHE", "blog" : "hahaha". "vk" : "1234"}
-import sys
+PASSWORDS = {"email" : "HEHEHE", "blog" : "hahaha", "vk" : "1234"}
+import sys, paperclip
 if len(sys.argv) < 2:
-    print()
+    print("Использование: ")
+    sys.exit()
+account = sys.argv[1]
+if account in PASSWORDS:
+    pyperclip.copy(Passwords[account])
+    print("Пароль для " + account +  " скопирован в буфер" )
+else:
+    print("Учетная запись " + account + "отсутствует в списке.")
+
